ab69e9ac1865b77aa004763ceb4f2ba459aed182
gcmurillo <gcmurillo@outlook.com>
2017-11-07 18:30:35 -0500

    Correccion de errores

--- Taller6Bootstrap/js/carrousel.js
+++ Taller6Bootstrap/js/carrousel.js
@@ -1,20 +1,19 @@
-$('#myCarousel').carousel({
-  interval: 4000
-})
+$('.multi-item-carousel').carousel({
+  interval: false
+});
 
-$('.carousel .item').each(function(){
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
   var next = $(this).next();
   if (!next.length) {
     next = $(this).siblings(':first');
   }
   next.children(':first-child').clone().appendTo($(this));
   
-  for (var i=0;i<2;i++) {
-    next=next.next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    
-    next.children(':first-child').clone().appendTo($(this));
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
   }
 });
